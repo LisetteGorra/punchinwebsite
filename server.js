3a7db0b8a81fd3874ece482228fb38e77de44fc6
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-03-27 17:58:30 -0400

    Required body parser, mognoose, express, and ejs

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+
+//creating express app, initializing express
+const app = express();
+
